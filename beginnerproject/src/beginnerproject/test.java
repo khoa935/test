07ea5958b77668abe3e0b6a9bec8a52c97c30372
khoa935 <nguyenthaitrikhoa2005@gmail.com>
2025-04-10 08:46:42 +0700

    second commit

--- beginnerproject/src/beginnerproject/test.java
+++ beginnerproject/src/beginnerproject/test.java
@@ -1,7 +1,9 @@
 package beginnerproject;
 
 public class test {
-
+void d() {
+	
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("hello world");

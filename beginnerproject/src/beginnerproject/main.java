c9784402bc1527dbd59a740172c9fbc2b20e1c9c
Windows 10 <nguyenthaitrikhoa2005@gmail.com>
2025-04-10 08:17:37 +0700

    second commit

---
+++ beginnerproject/src/beginnerproject/main.java
@@ -0,0 +1,46 @@
+package beginnerproject;
+import java.util.Scanner;
+//method overloading:la qua trinh trong cung 1 lop co nhieu pthuc cung ten nhung khac tham so
+//-khác số lượng tham so
+//-khac kieu du lieu cua tham so
+//ghi de:la qua trinh cac lop con thay doi lai dinh nghia cua lop cha
+//yeu cau cac phuong thuc duoc ghi de phai co cung ten va cung tham so voi cac phuong thuc cua lop cha
+//ham khoi tao:khoi tao gia tri cho cac bien thanh vien
+//cung ten voi lop
+//ko co kieu tra ve
+//đc gọi tu dong khi tao doi tuong
+//cai dat dc khai niem overloading
+//DA HINH:la kha nang 1 doi tuong co cac phuong thuc khac nhau,trong cac class khac nhau
+//class truu tuong la class chua hoan chinh co chua it nhat 1 method truu tuong
+//method truu tuong la cac method chi co phan khai bao,ko co phan cai dat
+//lop truu tuong ko tao dc doi tuong
+//lop truu tuong dc tao ra de lam lop co so(lop cha)
+//cac lop con su dung dc(tao dc đoi tuong)bat buoc phai cai dat tat ca cac method truu tuong
+//chua cac phuong thuc truu tuong(chi co phan khai bao,ko co phan cai dạt)neu cac pt co phan cai dat thi bat buoc phai khai bao tu khoa static
+//interface:cac thuoc tinh bat buoc khai bao voi tu khoa final hoac static
+//1 lop co the cai dạt(thua ke)nhieu hon 1 interface 
+//1 lop co the thua ke 1 lop khac va cai dat nhieu hon 1 interface
+//1 interface co the cai dat(thua ke)nhieu hon 1 interface
+//cac lop cai dat  interface bat buoc phai dinh nghia(cai dat tat ca cac phuong thuc ở interface do)
+interface I1{
+	int add(int a,int b);
+}
+//1 lop cai dat interface
+interface I2{
+	int sub(int a,int b);
+}
+interface I3{
+	int mul(int a,int b);
+}
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          
+          
+}
+}
+//do an ca nhan quan li gì:quan li thu vien
+//tim ít  nhat 2 pt truu tuong:theloaisach(),thoihanmuonsach();

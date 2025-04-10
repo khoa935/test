33b4fa75eea08bcc5d9068789cbe9fc7ed2cb54b
khoa935 <nguyenthaitrikhoa2005@gmail.com>
2025-04-10 08:48:05 +0700

    sc

--- beginnerproject/src/beginnerproject/test.java
+++ beginnerproject/src/beginnerproject/test.java
@@ -1,9 +1,7 @@
 package beginnerproject;
 
 public class test {
-void d() {
-	
-}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("hello world");
